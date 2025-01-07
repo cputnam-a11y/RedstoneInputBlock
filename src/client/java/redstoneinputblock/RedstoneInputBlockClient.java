c2019f956b93c1ab3a8fb0cf18c3e7f663b1fc91
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-01-06 19:39:20 -0600

    Change to axis block Datagen Nightmare

--- src/client/java/redstoneinputblock/RedstoneInputBlockClient.java
+++ src/client/java/redstoneinputblock/RedstoneInputBlockClient.java
@@ -1,10 +1,16 @@
 package redstoneinputblock;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import redstoneinputblock.block.ModBlocks;
+import redstoneinputblock.tint.RedstoneInputBlockColorProvider;
 
 public class RedstoneInputBlockClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        ColorProviderRegistry.BLOCK.register(
+                new RedstoneInputBlockColorProvider(),
+                ModBlocks.REDSTONE_INPUT_BLOCK
+        );
+    }
 }
--- src/client/java/redstoneinputblock/datagen/ModModelProvider.java
+++ src/client/java/redstoneinputblock/datagen/ModModelProvider.java
@@ -2,8 +2,11 @@ package redstoneinputblock.datagen;
 
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.client.data.*;
 import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 import redstoneinputblock.block.ModBlocks;
 import redstoneinputblock.block.RedstoneInputBlock;
 
@@ -40,20 +43,32 @@ public class ModModelProvider extends FabricModelProvider {
         }
         //endregion
         //region Block States
-        var map = BlockStateVariantMap.create(RedstoneInputBlock.SIGNAL);
         var baseId = Registries.BLOCK.getId(ModBlocks.REDSTONE_INPUT_BLOCK);
-        for (int value : RedstoneInputBlock.SIGNAL.getValues()) {
-            map.register(
-                    value,
-                    BlockStateVariant.create()
-                            .put(
-                                    VariantSettings.MODEL,
-                                    baseId
-                                            .withPrefixedPath("block/")
-                                            .withSuffixedPath(suffixes[value])
-                            )
-            );
-        }
+        var map = BlockStateVariantMap.create(RedstoneInputBlock.SIGNAL, PillarBlock.AXIS)
+                .register(
+                        (signal, axis) -> BlockStateVariant.create()
+                                .put(
+                                        VariantSettings.MODEL,
+                                        baseId
+                                                .withPrefixedPath("block/")
+                                                .withSuffixedPath(suffixes[signal])
+                                )
+                                .put(
+                                        VariantSettings.X,
+                                        switch (axis) {
+                                            case X, Z -> VariantSettings.Rotation.R90;
+                                            default -> VariantSettings.Rotation.R0;
+                                        }
+                                )
+                                .put(
+                                        VariantSettings.Y,
+                                        axis != Direction.Axis.X
+                                        ? VariantSettings.Rotation.R0
+                                        : VariantSettings.Rotation.R90
+                                )
+                );
+
+
         blockStateModelGenerator.blockStateCollector.accept(
                 VariantsBlockStateSupplier
                         .create(ModBlocks.REDSTONE_INPUT_BLOCK)
@@ -78,9 +93,43 @@ public class ModModelProvider extends FabricModelProvider {
         );
         //endregion
     }
+
     //region Util
     private static TexturedModel.Factory makeSuffixedFactory(String suffix) {
-        return TexturedModel.makeFactory(block -> TextureMap.all(Registries.BLOCK.getId(block).withPrefixedPath("block/").withSuffixedPath(suffix)), Models.CUBE_ALL);
+        var baseKey = TextureKey.of("base");
+        var baseTopKey = TextureKey.of("base_top");
+        var overlayKey = TextureKey.of("overlay");
+        var overlayTopKey = TextureKey.of("overlay_top");
+        return TexturedModel.makeFactory(
+                block -> {
+                    var id = Registries.BLOCK.getId(block).withPrefixedPath("block/");
+                    return new TextureMap()
+                            .put(
+                                    baseKey,
+                                    id.withSuffixedPath("_base")
+                            )
+                            .put(
+                                    baseTopKey,
+                                    id.withSuffixedPath("_base_top")
+                            )
+                            .put(
+                                    overlayKey,
+                                    id.withSuffixedPath(suffix).withSuffixedPath("_overlay")
+                            )
+                            .put(
+                                    overlayTopKey,
+                                    id.withSuffixedPath(suffix).withSuffixedPath("_overlay_top")
+                            );
+                },
+                new Model(
+                        Optional.of(Identifier.of(redstoneinputblock.RedstoneInputBlock.MOD_ID, "block/base_overlayable_axis_block")),
+                        Optional.empty(),
+                        baseKey,
+                        baseTopKey,
+                        overlayKey,
+                        overlayTopKey
+                )
+        );
     }
     //endregion
 }
